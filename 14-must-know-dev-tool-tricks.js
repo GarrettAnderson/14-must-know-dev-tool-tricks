ba8947d07b487e368c8b8531a103e4a67e4d5c96
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2021-04-02 00:59:57 -0400

    added notes from wes bos

---
+++ 14-must-know-dev-tool-tricks.js
@@ -0,0 +1,10 @@
+// I
+// JS running on the page but i dont kno2w what is causing it???
+
+/*
+
+Access the DOM and find the relevant element node. Right Click and select Break on.. then select attribute modification. This will show the specific JS
+
+*/
+
+// II
