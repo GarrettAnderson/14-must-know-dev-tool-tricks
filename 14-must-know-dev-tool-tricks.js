d90ff41e765e9509382b797f378c78e8ca12fe5e
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2021-04-21 14:04:18 -0400

    adding notes from wes bos video

--- 14-must-know-dev-tool-tricks.js
+++ 14-must-know-dev-tool-tricks.js
@@ -1,10 +1,29 @@
 // I
-// JS running on the page but i dont kno2w what is causing it???
+// JS running on the page but i dont know what is causing it???
 
 /*
 
-Access the DOM and find the relevant element node. Right Click and select Break on.. then select attribute modification. This will show the specific JS
+Click 'inspect element' or corresponding choice per web beowser in order to access the DOM and find the relevant element node. Right Click and select 'break on' then select attribute modification. This will show the specific JS
 
 */
 
 // II
+// Misc. Options
+
+function makeGreen() {
+  const p = document.querySelector('p')
+  p.style.color = '#BADA55'
+  p.style.fontSize = '50px'
+}
+
+
+
+// 1. Regular
+console.log('hello')
+
+// 2. Interpolate values into console.log but depracted and better to use back ticks ``
+console.log('Hello I am a %s string!', 'testing')
+// 'Hello I am a testing string!'
+
+// 3. Include ad hoc styles directly in console.log;
+console.log('%c I am some great text', 'font-size: 50px; background:red')
