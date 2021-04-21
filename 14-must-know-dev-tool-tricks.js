b11ea27bf772e7f390a5016ace18128ab5453d3e
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2021-04-21 14:59:51 -0400

    adding more tips and tricks on JS from wesbos 30 day JS course

--- 14-must-know-dev-tool-tricks.js
+++ 14-must-know-dev-tool-tricks.js
@@ -21,31 +21,71 @@ function makeGreen() {
 // 1. Regular
 console.log('hello')
 
+/*
+==========================
+==========================
+*/
+
 // 2. Interpolate values into console.log but depracted and better to use back ticks ``
 console.log('Hello I am a %s string!', 'testing')
 // 'Hello I am a testing string!'
 
+/*
+==========================
+==========================
+*/
+
 // 3. Include ad hoc styles directly in console.log;
 console.log('%c I am some great text', 'font-size: 50px; background:red, text-shadow: 10px 10px aqua')
+
+/*
+==========================
+==========================
+*/
 
 // 4. Print a WARNING
 console.warn('OH NOOO!')
 // Gives the stack trace (where in the file the warning is being called) of where it got called
 
+/*
+==========================
+==========================
+*/
+
 // 5. Error :|
 console.error('SHIT')
 // Gives the stack trace (where in the file the warning is being called) of where it got called
 
+/*
+==========================
+==========================
+*/
+
 // 6. Info
 console.info('Crocodiles eat 3 to 4 people per year!')
+
+/*
+==========================
+==========================
+*/
 
 // 7. Assertions
 console.assert( 1 === 1, 'This is wrong')
 // Will only fire if the assertion is false. If the assertion is true, nothing will happen.
 
+/*
+==========================
+==========================
+*/
+
 // 8. Clearing
 console.clear()
 // Will clear your console
+
+/*
+==========================
+==========================
+*/
 
 // 9. Viewing DOM Elements
 // console.log(p)
@@ -53,3 +93,28 @@ console.clear()
 // VS.
 
 // console.dir(p) => allows you to access a list of methods and properties that lives on that specific element
+
+/*
+==========================
+==========================
+*/
+
+// 10. Grouping together
+// 'dogs' is an object of dog objects
+
+dogs.forEach(dog => {
+  console.group(`${dog.name}`)
+  // OR console.groupCollapsed(`{dog.name}`)
+
+    console.log(`This is ${dog.name}`)
+    console.log(`${dog.name} is ${dog.age} years young.`)
+    console.log(`${dog.name} is well-mannered.`)
+
+  console.groupEnd(`${dog.name}`)
+})
+
+// 11. Counting
+
+
+
+// 12. Timing
